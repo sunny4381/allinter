ba804b40c59969c410a56bfae0f19731c9a4c34e
NAKANO Hideo <pinarello.marvel@gmail.com>
2020-05-24 13:39:42 +0900

    outputs results

--- src/main/java/allinter/Linter.java
+++ src/main/java/allinter/Linter.java
@@ -1,15 +1,29 @@
 package allinter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.actf.visualization.engines.lowvision.LowVisionException;
 import org.eclipse.actf.visualization.engines.lowvision.LowVisionType;
+import org.eclipse.actf.visualization.engines.lowvision.image.ImageException;
+import org.eclipse.actf.visualization.eval.problem.IProblemItem;
 
-import java.io.File;
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
+import java.util.List;
+
+import static com.google.common.io.Files.getFileExtension;
+import static java.nio.file.Files.createTempDirectory;
+import static java.nio.file.Files.walk;
 
 public class Linter implements Runnable {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final Charset UTF_8 = StandardCharsets.UTF_8;
+
     private final BrowserTab browser;
     private final String url;
     private final HtmlCheckerOptions htmlCheckerOptions;
@@ -38,25 +52,31 @@ public class Linter implements Runnable {
             return;
         }
 
-        Path workDirectory = Files.createTempDirectory("htmlchecker");
+        Path workDirectory = createTempDirectory("htmlchecker");
         try {
             var checker = new allinter.htmlchecker.Checker(browser, this.url, workDirectory.toString());
             checker.run();
+
+            outputResults(checker.getProblemList(), this.htmlCheckerOptions.getOutputReportFilepath());
         } finally {
-            Files.walk(workDirectory)
+            walk(workDirectory)
                     .sorted(Comparator.reverseOrder())
                     .map(Path::toFile)
                     .forEach(File::delete);
         }
     }
 
-    private void runLowVisionLint() throws IOException, LowVisionException {
+    private void runLowVisionLint() throws IOException, LowVisionException, ImageException {
         if (! this.lowVisionOptions.isLowvision()) {
             return;
         }
 
         var checker = new allinter.lowvision.Checker(browser, this.url, createLowVisionType());
         checker.run();
+
+        outputResults(checker.getProblemList(), this.lowVisionOptions.getOutputReportFilepath());
+        outputImage(checker.getLowvisionImage(), this.lowVisionOptions.getOutputImageFilepath());
+        outputImage(checker.getSourceImage(), this.lowVisionOptions.getSourceImageFilepath());
     }
 
     private LowVisionType createLowVisionType() throws LowVisionException {
@@ -76,5 +96,38 @@ public class Linter implements Runnable {
         }
 
         return lowVisionType;
+    }
+
+    private void outputResults(final List<IProblemItem> problemList, final Path outputFilepath) throws IOException {
+        if (outputFilepath == null) {
+            return;
+        }
+
+        ensureToExistDirectory(outputFilepath.getParent());
+
+        try (PrintWriter writer = new PrintWriter(outputFilepath.toFile(), UTF_8)) {
+            for (IProblemItem problemItem : problemList) {
+                writer.println(MAPPER.writeValueAsString(problemItem));
+            }
+        }
+    }
+
+    private void outputImage(final BufferedImage image, final Path outputFilepath) throws IOException {
+        if (outputFilepath != null) {
+            return;
+        }
+
+        ensureToExistDirectory(outputFilepath.getParent());
+
+        String format = getFileExtension(outputFilepath.toString());
+        ImageIO.write(image, format, outputFilepath.toFile());
+    }
+
+    private void ensureToExistDirectory(final Path dir) throws IOException {
+        if (dir == null || Files.exists(dir)) {
+            return;
+        }
+
+        Files.createDirectories(dir);
     }
 }
